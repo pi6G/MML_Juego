4f918ffc14b7bad16ecbd952c2d41b2cbdf43e35
GALIO <juandavid199er@gmail.com>
2024-01-27 14:54:23 -0500

    Merge

--- El bufon del rey/Assets/Scripts/GameManager.cs
+++ El bufon del rey/Assets/Scripts/GameManager.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public int score = 0;
+    public static int score2 = 0;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
