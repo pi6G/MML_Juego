e630abdbc650c05bfb2db21bb1dd788f5a35d50f
GALIO <juandavid199er@gmail.com>
2024-01-27 12:22:50 -0500

    Cargar los chistes: -Se implemento la logica para que los chistes se extraigan del rey y se carguen al iniciar la escena -Se creo la funcion para actualizar y pasar al siguiente trio de chistes

--- El bufon del rey/Assets/Scripts/UI/ButtonManager.cs
+++ El bufon del rey/Assets/Scripts/UI/ButtonManager.cs
@@ -2,11 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using TMPro;
 
 public class ButtonManager : MonoBehaviour
 {
     private Joke joke;
+    [SerializeField] private TMP_Text textUI;
     
+    public void UpdateJoke(Joke joke)
+    {
+        this.joke = joke;
+        textUI.text = joke.GetJokeContent();
+    }
 
     public void MakeTransition()
     {
