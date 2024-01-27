c19d5ebb99c65ce801ae63a321ac433e375c64c7
GALIO <juandavid199er@gmail.com>
2024-01-27 10:40:07 -0500

    Creacion de las clase base

---
+++ El bufon del rey/Assets/Scripts/Joke.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Joke : MonoBehaviour
+{
+    private Category category;
+    private string jokeContent;
+
+}
---
+++ El bufon del rey/Assets/Scripts/King.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class King : MonoBehaviour
+{
+    private Joke jokesList;
+
+}
