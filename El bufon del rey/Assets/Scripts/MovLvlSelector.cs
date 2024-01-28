91862a3598a857670bc867cdc1b5084551ada195
JulianG <julian2170789@correo.uis.edu.co>
2024-01-28 09:51:42 -0500

    Update movselector

--- El bufon del rey/Assets/Scripts/MovLvlSelector.cs
+++ El bufon del rey/Assets/Scripts/MovLvlSelector.cs
@@ -23,11 +23,11 @@ public class MovLvlSelector : MonoBehaviour
         StartCoroutine(Castle2());
     }
 
-    public void ToCastle3()
-    {
-        transform.DOMove(castle[2].position, 1).SetEase(Ease.OutCubic);
-        StartCoroutine(Castle3());
-    }
+    //public void ToCastle3()
+    //{
+        //transform.DOMove(castle[2].position, 1).SetEase(Ease.OutCubic);
+        //StartCoroutine(Castle3());
+    //}
 
     IEnumerator Castle1() 
     {
@@ -48,14 +48,5 @@ public class MovLvlSelector : MonoBehaviour
             SceneManager.LoadScene("BigQueen");
         }
     }
-
-    IEnumerator Castle3()
-    {
-        yield return new WaitForSeconds(1.5f);
-        if (ScoreManager.Instance.ValidateCastle(3))
-        {
-            ScoreManager.Instance.castles[2] = 0;
-            SceneManager.LoadScene("BigKing");
-        }
-    }
+    
 }
